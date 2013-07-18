b75599518760cb45ae53214cb0c8788a86366be6
Suyash Sadh <syscraftavnish@Suyash-Sadh-2.local>
2013-07-18 15:17:28 +0530

    Initial Commit

---
+++ SimpleMegnifire/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleMegnifire
+//
+//  Created by Suyash Sadh on 18/07/13.
+//  Copyright (c) 2013 Avnish Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
